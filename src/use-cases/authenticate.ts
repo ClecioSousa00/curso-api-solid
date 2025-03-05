ba559365f1148e74ff43e283104a2a41c3466c8b
ClecioSousa000 <cleciosouza015@gmail.com>
2025-03-05 13:27:48 -0300

    Feat: aula 84

--- src/use-cases/authenticate.ts
+++ src/use-cases/authenticate.ts
@@ -12,7 +12,7 @@ interface AuthenticateUseCaseResponse {
   user: User
 }
 
-export class RegisterUseCase {
+export class AuthenticateUseCase {
   constructor(private usersRepository: UsersRepository) {}
 
   async execute({
