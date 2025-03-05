ebe1fa2bf0923424392f1eb40b2ad0e3e3f1ebdd
ClecioSousa000 <cleciosouza015@gmail.com>
2025-03-05 14:39:39 -0300

    Feat: aula 88

---
+++ src/use-cases/check-in.spec.ts
@@ -0,0 +1,21 @@
+import { describe, it, expect, beforeEach } from 'vitest'
+import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+import { CheckInUseCase } from './check-In'
+
+let usersRepository: InMemoryCheckInsRepository
+let checkInsUseCase: CheckInUseCase
+describe('Check-in Use Case', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryCheckInsRepository()
+    checkInsUseCase = new CheckInUseCase(usersRepository)
+  })
+
+  it('should be able to check in', async () => {
+    const { checkIn } = await checkInsUseCase.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+})
