6baf87e2319a4ec77f8b8d2c0348b00129a2a091
ClecioSousa000 <cleciosouza015@gmail.com>
2025-03-13 21:06:15 -0300

    Feat: aula 91

--- src/use-cases/check-In.ts
+++ src/use-cases/check-In.ts
@@ -2,6 +2,7 @@ import { CheckIn } from '@prisma/client'
 import { CheckInsRepository } from '@/repositories/check-ins-repository'
 import { GymsRepository } from '@/repositories/gyms-repository'
 import { ResourceNotFoundError } from './errors/resource-not-found'
+import { getDistanceBetweenCoordinates } from '@/utils/get-distance-between-coordenate'
 
 interface CheckInUseCaseRequest {
   userId: string
@@ -30,6 +31,18 @@ export class CheckInUseCase {
 
     if (!gym) {
       throw new ResourceNotFoundError()
+    }
+
+    const distance = getDistanceBetweenCoordinates(
+      { latitude: userLatitude, longitude: userLongitude },
+      {
+        latitude: gym.latitude.toNumber(),
+        longitude: gym.longitude.toNumber(),
+      },
+    )
+    const MAX_DISTANCE_IN_kM = 0.1
+    if (distance > MAX_DISTANCE_IN_kM) {
+      throw new Error()
     }
 
     const checkInOnSameDay = await this.checkInsRepository.findByUserIdOnDate(
