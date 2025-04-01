bbceeadaf18bd440f1973d032bf52a687bb43891
ClecioSousa000 <cleciosouza015@gmail.com>
2025-03-31 21:21:27 -0300

    Feat: aula 113

--- src/http/controllers/gyms/search.ts
+++ src/http/controllers/gyms/search.ts
@@ -8,7 +8,7 @@ export async function search(request: FastifyRequest, reply: FastifyReply) {
     page: z.coerce.number().min(1).default(1),
   })
 
-  const { page, query } = searchQueryParams.parse(request.params)
+  const { page, query } = searchQueryParams.parse(request.query)
 
   const searchGymUseCase = makeSearchGymsUseCase()
   const { gyms } = await searchGymUseCase.execute({
