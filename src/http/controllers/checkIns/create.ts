57493e4753546bf83d732875a9a948c8d9f31c1d
ClecioSousa000 <cleciosouza015@gmail.com>
2025-04-01 20:16:35 -0300

    Feat: aula 115

--- src/http/controllers/checkIns/create.ts
+++ src/http/controllers/checkIns/create.ts
@@ -7,17 +7,17 @@ export async function create(request: FastifyRequest, reply: FastifyReply) {
     gymId: z.string().uuid(),
   })
   const createCheckInBodySchema = z.object({
-    latitude: z.number().refine((value) => {
+    latitude: z.coerce.number().refine((value) => {
       return Math.abs(value) <= 90
     }),
-    longitude: z.number().refine((value) => {
+    longitude: z.coerce.number().refine((value) => {
       return Math.abs(value) <= 180
     }),
   })
 
   const { latitude, longitude } = createCheckInBodySchema.parse(request.body)
 
-  const { gymId } = createCheckInParamsSchema.parse(request.query)
+  const { gymId } = createCheckInParamsSchema.parse(request.params)
 
   const createCheckInUseCase = makeCheckInUseCase()
   await createCheckInUseCase.execute({
