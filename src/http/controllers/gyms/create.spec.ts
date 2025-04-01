3b8ec2bf6353e5e0b5174a6ac55e55aaf087bdce
ClecioSousa000 <cleciosouza015@gmail.com>
2025-04-01 19:40:34 -0300

    Feat: aula 114

--- src/http/controllers/gyms/create.spec.ts
+++ src/http/controllers/gyms/create.spec.ts
@@ -3,7 +3,7 @@ import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-
 import request from 'supertest'
 import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-describe('profile (e2e)', () => {
+describe('Create Gym (e2e)', () => {
   beforeAll(async () => {
     await app.ready()
   })
@@ -12,19 +12,20 @@ describe('profile (e2e)', () => {
     await app.close()
   })
 
-  it('should be able to get user profile ', async () => {
+  it('should be able to create a gym ', async () => {
     const { token } = await createAndAuthenticateUser(app)
 
-    const profileResponse = await request(app.server)
-      .get('/profile')
+    const response = await request(app.server)
+      .post('/gyms')
       .set('Authorization', `Bearer ${token}`)
-      .send()
+      .send({
+        title: 'gym - teste',
+        description: 'teste',
+        phone: '12345678901',
+        latitude: -27.2092052,
+        longitude: -49.6401091,
+      })
 
-    expect(profileResponse.statusCode).toEqual(200)
-    expect(profileResponse.body.user).toEqual(
-      expect.objectContaining({
-        email: 'jonhdoe@gmail.com',
-      }),
-    )
+    expect(response.statusCode).toEqual(201)
   })
 })
